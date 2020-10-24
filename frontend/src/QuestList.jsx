72f7f479dc2743063997b1c6476b45eee5e9936f
Wojciech Kwolek <wojciech@kwolek.xyz>
2020-10-25 00:31:22 +0200

    use Sergiusz's Quest component

--- frontend/src/QuestList.jsx
+++ frontend/src/QuestList.jsx
@@ -1,36 +1,15 @@
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 
 import * as Ons from "react-onsenui"
 
-function getQuest(id) {
-    return fetch(`/api/v1/quests/${id}.json`).then(data => data.json())
-}
+import Quest from "./Quest"
 
-function QuestView({route, navigator}) {
-    const quest = route.quest;
-    const [questDetails, setQuestDetails] = useState(null);
-    console.log("QV", route)
-
-    useEffect(() => {
-        getQuest(quest.id).then(response => {
-            setQuestDetails(response.quest)
-        });
-    }, [quest.id])
-
-    return <Ons.Page>
-        <h1>{quest.title}</h1>
-        {questDetails === null ? <h3>Loading...</h3> : (
-            <p>{questDetails.instruction}</p>
-        )}
-        <a onClick={() => {navigator.popPage()}}>Back</a>
-    </Ons.Page>
-}
 
 function getQuests() {
     return fetch('/api/v1/quests.json').then(data => data.json())
 }
 
-function QuestList({navigator}) {
+function QuestList({ navigator }) {
     const [quests, setQuests] = useState([]);
     console.log(navigator)
     useEffect(() => {
@@ -39,15 +18,21 @@ function QuestList({navigator}) {
         })
     }, [])
 
-    return (<Ons.Page>
+    return (<Ons.Page
+
+        renderToolbar={() =>
+            <Ons.Toolbar>
+                <div className="center">Today's quests</div>
+            </Ons.Toolbar>}
+    >
         {quests.map(quest => (
             <Ons.Card
                 key={quest.id}
                 onClick={() => {
-                    navigator.pushPage({view: QuestView, title: "Quest", quest});
+                    navigator.pushPage({ view: Quest, quest });
                 }}>
-                <div className="title">{quest.title}<div style={{"float": "right"}}><Ons.Icon icon={quest.icon} /></div></div>
-                {quest.img && <div><img src={quest.img} style={{width: "100%"}} /></div>}
+                <div className="title">{quest.title}<div style={{ "float": "right" }}><Ons.Icon icon={quest.icon} /></div></div>
+                {quest.img && <div><img src={quest.img} style={{ width: "100%" }} /></div>}
             </Ons.Card>
         ))}
     </Ons.Page>);
