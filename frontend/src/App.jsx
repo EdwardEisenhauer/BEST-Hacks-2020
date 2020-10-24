f515ecd770adf03d8a649aa93a522911bb19f47c
Wojciech Kwolek <wojciech@kwolek.xyz>
2020-10-24 21:53:48 +0200

    F: add bottom tabbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import * as Ons from "react-onsenui"
 
 import './App.css';
@@ -7,7 +7,9 @@ import QuestList from "./QuestList"
 
 
 function App() {
-    const [route, setRoute] = useState({ view: QuestList, title: "Your Quests" }); // TODO: change this into an enum
+    const [route, setRoute] = useState({view: QuestList, title: "Your Quests"}); // TODO: change this into an enum
+    const [tabbarIndex, setTabbarIndex] = useState(0);
+
     return (
         <Ons.Page renderToolbar={() =>
             <Ons.Toolbar>
@@ -17,7 +19,21 @@ function App() {
             </Ons.Toolbar>
 
         }>
-            <route.view route={route} setRoute={setRoute} />
+            <Ons.Tabbar
+                onPreChange={({index}) => setTabbarIndex(index)}
+                index={tabbarIndex}
+                position='bottom'
+                renderTabs={(activeIndex, tabbar) => [
+                    {
+                        content: <Ons.Page><route.view setRoute={setRoute} route={route} /></Ons.Page >,
+                        tab: <Ons.Tab label="Quests" icon="md-check-all" />
+                    },
+
+                    {
+                        content: <Ons.Page><h1>kek</h1></Ons.Page>,
+                        tab: <Ons.Tab label="Settings" icon="md-settings" />
+                    }
+                ]} />
         </Ons.Page>
     );
 }
