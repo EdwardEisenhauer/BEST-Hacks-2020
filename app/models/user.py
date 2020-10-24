eb8b02756758ab3cad1b6d4f008f6bf431f5a4fa
Kacper Chmielewski <=>
2020-10-24 22:11:52 +0200

    add alembic stuff

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,7 @@ from app.models import Base
 
 
 class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
     email = Column(String(120), unique=True, nullable=False)
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
+from app.database import Base
+
 main_app = Blueprint('main_app', __name__,
                      template_folder='templates')
 
@@ -16,3 +18,15 @@ def show(page):
         return render_template('pages/%s.html' % page)
     except TemplateNotFound:
         abort(404)
+
+
+@main_app.route('/users')
+def users():
+    from app.models.user import User
+    admin = User(username='admin', email='admin@example.com')
+    guest = User(username='guest', email='guest@example.com')
+
+    Base.session.add(admin)
+    Base.session.add(guest)
+
+    return "witam swiat :))"
