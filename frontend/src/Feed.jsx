911724445a31f9a094da538b4ec89ab19fab7c97
sergiusz <sergiusz.warga@gmail.com>
2020-10-25 02:35:41 +0200

    Introduced simple Feed logic

--- frontend/src/Feed.jsx
+++ frontend/src/Feed.jsx
@@ -16,22 +16,24 @@ function Feed({ navigator }) {
         })
     }, [])
 
-    return (<Ons.Page
+    return <Ons.Page
         renderToolbar={
             () => <Ons.Toolbar>
                 <div className="center">Feed</div>
             </Ons.Toolbar>
         }>
-        {posts.map((post) => (
-            <Ons.Card
+        {posts.map((post) => <Ons.Card
                 key={post.id}
                 onClick={() => {
                     // TODO: implement navigator for this tab and  postview and fix this
                 }}>
+            { post.type === "activity" ?
+                <div className="title">{post.user + " " + post.title}</div>
+                :
                 <div className="title">{post.title}</div>
-            </Ons.Card>
-        ))}
-    </Ons.Page>);
+            }
+            </Ons.Card>)}
+    </Ons.Page>;
 }
 
 export default Feed;
