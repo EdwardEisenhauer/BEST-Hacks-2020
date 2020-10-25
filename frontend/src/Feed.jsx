414dc7915c59be507c7270f663b8b6128eee1ee1
Wojciech Kwolek <wojciech@kwolek.xyz>
2020-10-25 07:04:18 +0100

    Style the feed

--- frontend/src/Feed.jsx
+++ frontend/src/Feed.jsx
@@ -1,10 +1,41 @@
 import * as Ons from "react-onsenui"
 
+import CardActions from "./CardActions"
+
 import { useState, useEffect } from "react";
+import { render } from "react-dom";
 
 // TODO: implement getPosts
 function getPosts() {
     return fetch('/api/posts').then(data => data.json())
+}
+
+function renderPost(post) {
+    switch (post.type) {
+        case "activity":
+            return <>
+                <div style={{ fontSize: "1.2em", paddingBottom: ".5em" }}>
+                    <span style={{ fontWeight: "bold" }}>{post.user}</span> {post.title}
+                </div>
+            </>
+        case "tip":
+            return <>
+                <div style={{ fontSize: "1.2em", paddingBottom: ".5em" }}>
+                    {post.title}
+                </div>
+            </>
+        case "quest":
+            return <>
+                <div style={{ fontSize: "1em" }}>
+                    <span style={{ fontWeight: "bold" }}>{post.user}</span> {post.title}
+                </div>
+                <div style={{ fontSize: "1.5em", paddingTop: ".4em", paddingBottom: "0.5em" }}>
+                    {post.quest.title}
+                </div>
+            </>
+        default:
+            return post.title
+    }
 }
 
 function Feed({ navigator }) {
@@ -12,6 +43,7 @@ function Feed({ navigator }) {
 
     useEffect(() => {
         getPosts().then((response) => {
+            console.log(response.posts)
             setPosts(response.posts);
         })
     }, [])
@@ -27,11 +59,14 @@ function Feed({ navigator }) {
             onClick={() => {
                 // TODO: implement navigator for this tab and  postview and fix this
             }}>
-            {post.type === "activity" ?
-                <div className="title">{post.user + " " + post.title}</div>
-                :
-                <div className="title">{post.title}</div>
-            }
+            {renderPost(post)}
+            {post.img && <img style={{ width: "100%", borderRadius: 5 }} src={post.img}></img>}
+            {post.text && <div style={{ paddingTop: post.img != null ? ".5em" : "0" }}>{post.text}</div>}
+            <CardActions left={<>
+                <Ons.Icon icon="md-thumb-up" size={24} />
+            </>}>
+                <a>Więcej...</a>
+            </CardActions>
         </Ons.Card>)}
     </Ons.Page>;
 }
