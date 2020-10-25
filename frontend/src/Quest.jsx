8a9c59526439863d973c0724fc7f197075c61f88
Wojciech Kwolek <wojciech@kwolek.xyz>
2020-10-25 02:37:11 +0100

    Quest.jsx design

--- frontend/src/Quest.jsx
+++ frontend/src/Quest.jsx
@@ -1,6 +1,8 @@
 import * as Ons from "react-onsenui"
 
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+
+import CardActions from "./CardActions"
 
 function getQuest(id) {
     return fetch(`/api/v1/quests.json`).then(data => data.json()).then(data => ({
@@ -8,7 +10,7 @@ function getQuest(id) {
     }))
 }
 
-function Quest({route}) {
+function Quest({ route, navigator }) {
     const quest = route.quest;
     const [questDetails, setQuestDetails] = useState(null);
     console.log("QV", route)
@@ -24,25 +26,43 @@ function Quest({route}) {
             <div class="center">Quest</div>
         </Ons.Toolbar>
     }>
-        {questDetails == null ? <h1>Loading...</h1> : <Ons.Card>
-            <div className="title">
-                {questDetails.title}
-                <div style={{"float": "right"}}>
-                    {questDetails.icon && <Ons.Icon icon={questDetails.icon} />}
-                </div>
-            </div> { /* TODO: Get title and category icon (some category -> icon name mapping?) from DB. */}
-            <div className="content">
-                {questDetails['additional note']}
-                {/* TODO: images with no link */}
-                {/* TODO: friend avatars */}
-                {questDetails.link && <a href={questDetails.link}><img
-                    src={questDetails.img} style={{"width": "100%"}}
-                    alt={questDetails.alt} /></a>}
-            Friends list:<br />
-                <img src={"https://avatars0.githubusercontent.com/u/25778208?s=460&u=0333a8b6bdcff8a8ad623cde86b767515513b09a&v=4"} style={{"width": "10%"}} alt={"Sergiusz"} /> { /* Add href to Users' profiles */}
-                <div style={{"float": "right"}}><Ons.Button id="button-quest-accept">Let{"'"}s do it!</Ons.Button></div>
-            </div>
-        </Ons.Card>}
+        {questDetails == null ? <h1>Loading...</h1> :
+            (<>
+                <Ons.Card style={{ padding: "1em" }}>
+                    <div style={{ color: "#555", fontSize: "0.8em" }}>
+                        <span style={{ padding: ".1em", paddingRight: ".5em" }}>{questDetails.icon && <Ons.Icon icon={questDetails.icon} />}</span>
+                        <span>{questDetails.category}</span>
+                    </div>
+                    <div className="title">
+                        <h1>{questDetails.title}</h1>
+                    </div>
+                    {questDetails.img && <div className="image" style={{ paddingBottom: "1em" }}>
+                        <img style={{ width: "100%", borderRadius: 5 }} src={questDetails.img}></img>
+                    </div>}
+                    <div className="content" style={{ paddingBottom: ".7em" }}>
+                        {questDetails['additional note']}
+                        {/* TODO: images with no link */}
+                        {/* TODO: friend avatars */}
+                    </div>
+                    {questDetails.link && <div className="link" style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>
+                        <a href={questDetails.link} style={{ color: "#3377ff" }} target="_blank" rel="norefer nofollow">{questDetails.link}</a>
+                    </div>}
+
+                    <CardActions>
+                        <a id="button-quest-accept" onClick={() => {
+                            navigator.pushPage({ view: null, quest: questDetails })
+                        }}>
+                            Mark as done
+                            </a>
+                    </CardActions>
+
+                </Ons.Card>
+                <Ons.Card>
+                    <div style={{ paddingBottom: ".4em", paddingTop: ".2em", fontWeight: "bold", fontSize: "0.9em", fontSize: "0.9em", color: "#222" }}>Znajomi, którzy ukończyli to zadanie:</div>
+                    <img src={"https://avatars0.githubusercontent.com/u/25778208?s=460&u=0333a8b6bdcff8a8ad623cde86b767515513b09a&v=4"} style={{ borderRadius: 5, "width": "2.5em", marginLeft: "0em", marginTop: ".3em" }} alt={"Sergiusz"} /> { /* Add href to Users' profiles */}
+                </Ons.Card>
+            </>)
+        }
     </Ons.Page >
 }
 
